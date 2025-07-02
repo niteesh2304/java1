4a82ff6deb4c99852e4ce1090ce8127af856f08f
niteesh2304 <svniteesh2004@gmail.com>
2025-07-02 20:37:36 +0530

    8:37PM 02-07-2025

--- array/remove_range.java
+++ array/remove_range.java
@@ -16,25 +16,34 @@ public class remove_range{
 		arr(nums,sc);
 		int count = count(nums,low,high);
 		int []update = new int[nums.length-count];
+		remove(nums,update,low,high);
 		}
-	public static void arr(int nums[],sc Scanner){
-		System.out.print("nums = ")
-		for(int i =0 ; i < nums.arr.length;i++){
-			num[i] = sc.nextInt();
+	public static void arr(int nums[],Scanner sc){
+		System.out.print("nums = ");
+		for(int i =0 ; i < nums.length;i++){
+			nums[i] = sc.nextInt();
 			}
 		System.out.println();
 		}
 	public static int count(int nums[],int low,int high){
-		count = 0;
-		for(int i = 0 ; i < nums.length-1;i++){
-			if(arr[i] > low && arr[i] < high){
+		int count = 0;
+		for(int i = 0 ; i < nums.length;i++){
+			if(nums[i] > low && nums[i] < high){
 				count++;
 				}
 			}
+		return count;
 		}
-	public static void remove(int nums[],int low,int high){
+	public static void remove(int nums[],int update[],int low,int high){
 		int j = 0;
-		for(int i = 0 ; i < nums.length-1;i++){
+		for(int i = 0 ; i < nums.length;i++){
+			if(low > nums[i] || high < nums[i]){
+				update[j] = nums[i];
+				j++; 
+				}
+		}
+		for(int i = 0 ; i < update.length;i++){
+			System.out.print(update[i]+" ");	
 			}
 		}
 	}
